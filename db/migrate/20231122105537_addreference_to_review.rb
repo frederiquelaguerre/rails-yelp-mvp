f3d159321c6aa407adec87f0ec46334d15a3036b
Frédérique Laguerre <laguerre.frederique@gmail.com>
2023-11-22 17:33:49 +0100

    yelp

---
+++ db/migrate/20231122105537_addreference_to_review.rb
@@ -0,0 +1,5 @@
+class AddreferenceToReview < ActiveRecord::Migration[7.1]
+  def change
+    add_reference :reviews, :restaurant, foreign_key: true
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,17 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'faker'
+
+Restaurant.destroy_all
+
+5.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.city,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: Restaurant::CATEGORY.sample
+  )
+  restaurant.save
+end
